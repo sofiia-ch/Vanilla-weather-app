f84cc002238754f2d68207a435ac3c13dfd359a8
sofiia-ch <chubasofiia@gmail.com>
2023-07-17 19:42:24 +0300

    worked on displayin the current weather parameters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector ("#humidity");
+  let windElement = document.querySelector ("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
 let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
 let units = "metric";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=${units}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Oslo&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(displayTemperature);
