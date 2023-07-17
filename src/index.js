f0632c3266734dc39a778cc2b70294ae28624979
sofiia-ch <chubasofiia@gmail.com>
2023-07-17 18:15:09 +0300

    added api

---
+++ src/index.js
@@ -0,0 +1,3 @@
+let apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
+let units = "metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=${units}`;
